11d75b11c39f7bf1c5eef4533461d8b895db2905
negita-rms <kazuma_negita@recruit-ms.co.jp>
2024-10-18 11:47:36 +0900

    feat: カウンター実装

--- components/AppAlert.vue
+++ components/AppAlert.vue
@@ -1,5 +1,14 @@
 <template>
-  <span>
-    <slot />
-  </span>
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+// function increment() {
+//   count.value++;
+// }
+const increment = () => count.value++
+</script>
