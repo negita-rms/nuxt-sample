cfcecf9d6e007dd2eb68e7c52c079d6a0bddbb8f
negita-rms <kazuma_negita@recruit-ms.co.jp>
2025-03-19 19:17:51 +0900

    feat: floating-vue試す

--- app.vue
+++ app.vue
@@ -1,10 +1,16 @@
+<script setup lang="ts">
+import { Dropdown, Tooltip, Menu, vTooltip } from 'floating-vue'
+
+
+</script>
+
 <template>
   <div>
     <h1>Welcome to the homepage</h1>
-    <!-- <AppAlert>
-      This is an auto-imported component.
-    </AppAlert> -->
     <ParentComponent />
     <Form />
+    <div>
+      <div v-tooltip="'Hello world'">Hover me</div>
+    </div>
   </div>
 </template>
